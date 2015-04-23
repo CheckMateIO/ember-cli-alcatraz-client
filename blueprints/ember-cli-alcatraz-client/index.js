bd7b7ce968cd9ebde27d0b50ca597cc692457f89
Adam Scott <ascot21@gmail.com>
2015-04-23 09:38:16 -0700

    Remove unneccessary code and update Readme

--- blueprints/ember-cli-alcatraz-client/index.js
+++ blueprints/ember-cli-alcatraz-client/index.js
@@ -1,10 +1,3 @@
 module.exports = {
 
-  beforeInstall: function(options) {
-    // return this.addBowerPackageToProject('alcatraz-client');
-  },
-
-  afterInstall: function() {
-    // return this.addBowerPackageToProject('alcatraz-client');
-  }
 };
